0534b202fb9f5605174907385e1efb2912584c97
Benjamin Liu <benjamin.liu@servicemax.com>
2023-06-14 16:57:47 +0800

    Test

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Huan Huan&apos;s Assistant</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Search everything from here!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
